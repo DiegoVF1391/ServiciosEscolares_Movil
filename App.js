c87a55de695fadf7b40eaf109ff917c145df6e68
lilchris1 <djlilchris.wtb@gmail.com>
2022-12-12 10:39:52 -0600

    login conectado a API v1

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
-export default function App() {
+// export default function App() {
   
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Hola!!</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
-}
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <Text>Hola!!</Text>
+//       <StatusBar style="auto" />
+//     </SafeAreaView>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'dodgerblue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'dodgerblue',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+import React from 'react';
+import {StatusBar, Text, View} from 'react-native';
+import Navigation from './src/components/Navigation';
+import {AuthProvider} from './src/context/AuthContext';
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <StatusBar backgroundColor="#06bcee" />
+      <Navigation />
+    </AuthProvider>
+  );
+};
+
+export default App;
